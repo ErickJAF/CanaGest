b3a07bd9552eef79c5ac9023adc408574ba3f5b3
isa1ascm <isaias.coronado252252@potros.itson.edu.mx>
2026-05-13 18:29:21 -0700

    infra estructura banco

---
+++ diseniadores-infraestructura-banco/src/main/java/diseniadores/infraestructura/banco/IBanco.java
@@ -0,0 +1,14 @@
+package diseniadores.infraestructura.banco;
+
+import diseñadores.infraestructura.dto.RespuestaBancoDTO;
+import diseñadores.infraestructura.dto.SolicitudBancoDTO;
+
+interface IBanco {
+
+  RespuestaBancoDTO procesarTarjeta(SolicitudBancoDTO solicitud);
+
+  RespuestaBancoDTO procesarTransferencia(SolicitudBancoDTO solicitud);
+
+  RespuestaBancoDTO procesarQR(SolicitudBancoDTO solicitud);
+
+}
---
+++ diseniadores-infraestructura-banco/src/main/java/diseniadores/infraestructura/banco/Main.java
@@ -0,0 +1,15 @@
+package diseniadores.infraestructura.banco;
+
+import java.io.IOException;
+
+public class Main {
+
+  public static void main(String[] args) {
+    try {
+      BancoServidor.iniciar(BancoCliente.PUERTO);
+    } catch (IOException ex) {
+      System.getLogger(Main.class.getName()).log(System.Logger.Level.ERROR, (String) null, ex);
+    }
+  }
+
+}
