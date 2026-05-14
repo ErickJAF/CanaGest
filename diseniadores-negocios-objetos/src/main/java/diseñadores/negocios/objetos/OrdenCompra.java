903ff467fa9e8d5bb12f96370f1e408f9a0eaba2
isa1ascm <isaias.coronado252252@potros.itson.edu.mx>
2026-05-14 11:57:42 -0700

    correcciones

--- diseniadores-negocios-objetos/src/main/java/diseñadores/negocios/objetos/OrdenCompra.java
+++ diseniadores-negocios-objetos/src/main/java/diseñadores/negocios/objetos/OrdenCompra.java
@@ -1,33 +1,32 @@
 package diseñadores.negocios.objetos;
 
 import diseñadores.negocios.dto.OrdenCompraDTO;
-import diseñadores.persistencia.IPersistencia;
-import diseñadores.persistencia.PersistenciaFacade;
+import diseñadores.persistencia.dao.IOrdenCompraDAO;
+import diseñadores.persistencia.dao.impl.OrdenCompraDAOImpl;
 
 import java.util.List;
 
 public class OrdenCompra {
 
-  private static final IPersistencia PERSISTENCIA = PersistenciaFacade.getInstancia();
+    private static final IOrdenCompraDAO ORDEN_COMPRA_DAO = new OrdenCompraDAOImpl();
 
-  public static List<OrdenCompraDTO> obtenerTodas() {
-    return PERSISTENCIA.obtenerOrdenesCompra();
-  }
+    public static List<OrdenCompraDTO> obtenerTodas() {
+        return ORDEN_COMPRA_DAO.obtenerTodas();
+    }
 
-  public static OrdenCompraDTO obtenerPorNumero(String numero) {
-    return PERSISTENCIA.obtenerOrdenCompraPorNumero(numero);
-  }
+    public static OrdenCompraDTO obtenerPorNumero(String numero) {
+        return ORDEN_COMPRA_DAO.obtenerPorNumero(numero);
+    }
 
-  public static void guardar(OrdenCompraDTO orden) {
-    PERSISTENCIA.guardarOrdenCompra(orden);
-  }
+    public static void guardar(OrdenCompraDTO orden) {
+        ORDEN_COMPRA_DAO.guardar(orden);
+    }
 
-  public static void actualizar(OrdenCompraDTO orden) {
-    PERSISTENCIA.actualizarOrdenCompra(orden);
-  }
+    public static void actualizar(OrdenCompraDTO orden) {
+        ORDEN_COMPRA_DAO.actualizar(orden);
+    }
 
-  public static void eliminar(String numero) {
-    PERSISTENCIA.eliminarOrdenCompra(numero);
-  }
-
+    public static void eliminar(String numero) {
+        ORDEN_COMPRA_DAO.eliminar(numero);
+    }
 }
--- diseniadores-negocios-objetos/src/main/java/diseñadores/negocios/objetos/Proveedor.java
+++ diseniadores-negocios-objetos/src/main/java/diseñadores/negocios/objetos/Proveedor.java
@@ -1,33 +1,32 @@
 package diseñadores.negocios.objetos;
 
 import diseñadores.negocios.dto.ProveedorDTO;
-import diseñadores.persistencia.IPersistencia;
-import diseñadores.persistencia.PersistenciaFacade;
+import diseñadores.persistencia.dao.IProveedorDAO;
+import diseñadores.persistencia.dao.impl.ProveedorDAOImpl;
 
 import java.util.List;
 
 public class Proveedor {
 
-  private static final IPersistencia PERSISTENCIA = PersistenciaFacade.getInstancia();
+    private static final IProveedorDAO PROVEEDOR_DAO = new ProveedorDAOImpl();
 
-  public static List<ProveedorDTO> obtenerTodos() {
-    return PERSISTENCIA.obtenerProveedores();
-  }
+    public static List<ProveedorDTO> obtenerTodos() {
+        return PROVEEDOR_DAO.obtenerTodos();
+    }
 
-  public static ProveedorDTO obtenerPorCodigo(String codigo) {
-    return PERSISTENCIA.obtenerProveedorPorCodigo(codigo);
-  }
+    public static ProveedorDTO obtenerPorCodigo(String codigo) {
+        return PROVEEDOR_DAO.obtenerPorCodigo(codigo);
+    }
 
-  public static void guardar(ProveedorDTO proveedor) {
-    PERSISTENCIA.guardarProveedor(proveedor);
-  }
+    public static void guardar(ProveedorDTO proveedor) {
+        PROVEEDOR_DAO.guardar(proveedor);
+    }
 
-  public static void actualizar(ProveedorDTO proveedor) {
-    PERSISTENCIA.actualizarProveedor(proveedor);
-  }
+    public static void actualizar(ProveedorDTO proveedor) {
+        PROVEEDOR_DAO.actualizar(proveedor);
+    }
 
-  public static void eliminar(String codigo) {
-    PERSISTENCIA.eliminarProveedor(codigo);
-  }
-
+    public static void eliminar(String codigo) {
+        PROVEEDOR_DAO.eliminar(codigo);
+    }
 }
